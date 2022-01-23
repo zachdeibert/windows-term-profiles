6e0d1493980c402aedd8cf07d9bc3554db7c743f
Zach Deibert <zachdeibert@gmail.com>
2022-01-23 10:40:11 -0600

    Created project

---
+++ Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.GitHub.ZachDeibert.WindowsTermProfiles {
+    public class Entry {
+        public static void Main() {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
